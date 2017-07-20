68b26849862cff4515de122d9ebfd78a404e52c3
mainaGioia <alessandra.manna89@gmail.com>
2017-07-20 17:20:17 +0200

    to edit - navigation to add

---
+++ app/components/EditTask/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+
+const styles= StyleSheet.create({
+  editTaskContainer: {
+
+  },
+  editTaskText: {
+    fontSize: 30,
+    color: '#333333'
+  }
+
+})
